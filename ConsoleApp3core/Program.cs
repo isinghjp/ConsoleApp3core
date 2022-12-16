2db05ebc27f8df48cf38c4673d8becf9909e3d20
imjays <jpkumar541@gmail.com>
2022-12-16 10:54:41 +0530

    Add project files.

---
+++ ConsoleApp3core/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string[] weekdays = new string[] { "Monday", "Tuesday", "Wednesday", "Thrusday", "Friday", "Saturday", "Sunday" };
+
+Range range = ..3;
+
+string[] sublist = weekdays[range];
+foreach(string ele in sublist)
+{
+    Console.WriteLine($"{ele}");
+}
+
+//Console.WriteLine($"a={weekdays[range]}");
+
+//Console.WriteLine("Hello, World!");
